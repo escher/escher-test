2f8df373c57abd81a4a59ca8d23d6f9351921276
Zachary King <zaking17@gmail.com>
2017-03-27 15:36:06 -0700

    more reaction shapes

--- src/reaction_shapes.js
+++ src/reaction_shapes.js
@@ -21,14 +21,22 @@ const first_load_callback = function () {
   const size = this.zoom_container.get_size()
 
   // Draw the reaction
-  this.map.new_reaction_from_scratch('ENO', { x: 0, y: 200 }, 90)
-  this.map.new_reaction_from_scratch('GAPD', { x: -200, y: 0 }, 180)
-  this.map.new_reaction_from_scratch('PGI', { x: 200, y: 0 }, 0)
-  this.map.new_reaction_from_scratch('CYTBD2pp', { x: 0, y: -200 }, -90)
-  this.map.new_reaction_from_scratch('NADTRHD', { x: 200, y: -200 }, -45)
-  this.map.new_reaction_from_scratch('XYLabcpp', { x: -200, y: -200 }, -135)
-  this.map.new_reaction_from_scratch('FUMt2_3pp', { x: 200, y: 200 }, 45)
-  this.map.new_reaction_from_scratch('PPA2', { x: -200, y: 200 }, 135)
+  const draw = (bigg_id, loc, rot) => {
+    this.map.new_reaction_from_scratch(bigg_id, loc, rot)
+    this.map.new_reaction_for_metabolite(
+      bigg_id, Object.keys(this.map.get_selected_nodes())[0], rot
+    )
+  }
+  draw('ENO', { x: 0, y: 200 }, 90)
+  draw('PPS', { x: -200, y: 0 }, 180)
+  draw('CYTBD2pp', { x: 200, y: 0 }, 0)
+  draw('PGI', { x: 0, y: -200 }, -90)
+  draw('NADTRHD', { x: 200, y: -200 }, -35)
+  draw('XYLabcpp', { x: -200, y: -200 }, -135)
+  draw('THZPSN3', { x: 600, y: 200 }, 45)
+  draw('PPA2', { x: -200, y: 200 }, 135)
+  this.map.new_reaction_from_scratch('Ec_biomass_iJO1366_WT_53p95M',
+                                     { x: 0, y: 1300 }, 90)
 
   // And zoom the map to focus on that reaction
   this.map.zoom_extent_nodes()
@@ -59,9 +67,19 @@ export default function draw () {
       enable_keys: true,
       fill_screen: true,
       never_ask_before_quit: true,
-      show_gene_reaction_rules: true,
+      show_gene_reaction_rules: false,
       full_screen_button: true,
       first_load_callback,
+      reaction_data: { PPS: 20, THZPSN3: 20 },
+      metabolite_data: { atp_c: 20 },
+      // reaction_no_data_size: 25,
+      // metabolite_no_data_size: 25,
+      canvas_size_and_loc: {
+        x: -4500,
+        y: -1000,
+        width: 9000,
+        height: 3500
+      }
     }
 
     Builder(null, model, null, d3.select('body'), options1)
